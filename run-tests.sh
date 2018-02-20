851e6eefc8596ed6a19d2578ab5ec209ad448918
Jacob Springer <jspring1@swarthmore.edu>
2018-02-20 12:39:18 -0500

    Add tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-make clean
-make
-cd tests/
-python3 ../../tools/morph-test.py -csi bkl.yaml
-cd ..
+make clean && make && cd tests/ && python3 ../../tools/morph-test.py -csi bkl.yaml && cd ..
