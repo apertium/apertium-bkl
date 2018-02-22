abccc1450308b0d64ef8400608d76adcf7b09be6
Daniel Swanson <dswanso1@swarthmore.edu>
2018-02-22 12:13:39 -0500

    add generation to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make clean && make && cd tests/ && python3 ../../tools/morph-test.py -csi bkl.yaml && cd ..
+make clean && make && cd tests/ && python3 ../../tools/morph-test.py -csi bkl.yaml ; python3 ../../tools/morph-test.py -cl bkl.yaml && cd ..
