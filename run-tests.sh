a63ffc812a2d0ca0f50ba96cbca31a5359392dc5
Daniel Swanson <dswanso1@swarthmore.edu>
2018-02-27 11:28:22 -0500

    handy-dandy output formatting

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make clean && make && cd tests/ && python3 ../../tools/morph-test.py -csi bkl.yaml ; python3 ../../tools/morph-test.py -cl bkl.yaml && cd ..
+make clean && make && cd tests/ && python3 ../../tools/morph-test.py -csi bkl.yaml ; python3 ../../tools/morph-test.py -cl bkl.yaml ; echo "\nfinal totals:" ; python3 ../../tools/morph-test.py -csi bkl.yaml | tail -1 ; python3 ../../tools/morph-test.py -cl bkl.yaml | tail -1 && cd ..
