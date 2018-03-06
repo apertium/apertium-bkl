7741636956d5d34272a3b30716ce059f6a8d3695
Daniel Swanson <dswanso1@swarthmore.edu>
2018-03-06 12:12:30 -0500

    pronoun fixes

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 make clean && make && aq-covtest ../ling073-bkl-corpus/bkl.corpus.basic.txt bkl.automorf.bin ; cd tests/ && python3 ../../tools/morph-test.py -csi bkl.yaml ; python3 ../../tools/morph-test.py -cl bkl.yaml ; echo "\nfinal totals:" ; python3 ../../tools/morph-test.py -csi bkl.yaml | tail -1 ; python3 ../../tools/morph-test.py -cl bkl.yaml | tail -1 ; cd .. ; aq-covtest ../ling073-bkl-corpus/bkl.corpus.basic.txt bkl.automorf.bin | sed -n 2p
+echo "Tokens in corpus:"
+cat ../ling073-bkl-corpus/bkl.corpus.basic.txt | lt-proc bkl.automorf.bin | sed 's/$\W*\^/$\n^/g' | wc -l
+echo "Analyses in corpus:"
+cat ../ling073-bkl-corpus/bkl.corpus.basic.txt | lt-proc bkl.automorf.bin | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l
